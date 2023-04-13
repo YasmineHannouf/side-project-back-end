e1812f391d35e928cbff364142bbab09a08a3e14
Yasmine Hannouf <yasmina.hannouf.2018@gmail.com>
2023-04-13 12:58:51 +0300

    add /

--- Backend/controlles/categoryContoller.js
+++ Backend/controlles/categoryContoller.js
@@ -1,39 +1,49 @@
 import categoryModel from "../models/categoryModel.js";
 
-// get category by id 
-export async function getCategoryById(req, res) {
-  let { id } = req.params;
-  categoryModel.findOne({ _id: id }, (err, response) => {
-          if (err) return next(err);
-          res.status(200).send({ success: true, response });
-      });
+
+
+
+function getCategoryById(req, res) {
+  const categoryId = req.params.id;
+  categoryModel.findOne({ _id: categoryId })
+    .then(category => {
+      if (!category) {
+        return res.status(404).send('Category not found');
+      }
+      res.send(category);
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).send('Internal server error');
+    });
 }
+
+
+
 // get all the categories 
 
-export async function getAllCategory (req,res,next){
-  categoryModel.find({},(err,response) =>{
-    if(err)return next (err);
-    return res.status(200).send({success:true,response});
-  });
+export  async function getAllCategory(req, res, next) {
+  // categoryModel.find({}, (err, response) => {
+  //   if (err) return next(err);
+  //   return res.status(200).send({ success: true, response });
+  // });
+ const categories= await categoryModel.find({}).exec();
+ if(categories){
+   return res.status(200).send({ success: true, data: categories });
+
+ }
+  
 }
 
 // create category
 export const createCategory = async (req, res) => {
   try {
-    const category = await categoryModel.findOne({ name: req.body.name ,
-    products:req.body.products});
-
-    if (category) {
-      return res.status(400).json({ msg: "Category already exists" });
-    }
-    const newCategory = new category(req.body);
-
-    await newCategory.save();
-
-    res.json(newCategory);
-  } catch (err) {
-    console.error(err.message);
-    res.status(500).send("server error");
+    const { name } = req.body;
+    const newCategory = await categoryModel.create({ name });
+    res.status(200).json({ success: true, data: newCategory });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ success: false, message: 'Server Error' });
   }
 };
 
@@ -46,7 +56,7 @@ export const updateCategory = async (req, res) => {
     if (!category) {
       return res.status(404).json({ msg: "Category not found" });
     }
-    category.name = name; 
+    category.name = name;
     await category.save();
     res.json(category);
   } catch (err) {
@@ -59,25 +69,18 @@ export const updateCategory = async (req, res) => {
 };
 // Delete a specific category
 
-export const deleteCategory = async (req, res) => {
+export const deleteCategory = async (req, res,next ) => {
+  const id = req.params.id;
   try {
-    const category = await categoryModel.findById(req.params.id);
-    if (!category) {
-      return res.status(404).json({ msg: "Category not found" });
-    }
-    await category.remove();
-    res.json({ msg: "Category removed" });
-  } catch (err) {
-    console.error(err.message);
-    if (err.kind === "ObjectId") {
-      return res.status(404).json({ msg: "Category not found" });
-    }
-    res.status(500).send("Server Error");
+    let response = await categoryModel.findByIdAndRemove({ _id: id });
+    res.status(200).send({ success: true, response });
+  } catch (error) {
+    res.status(400).send({ error: true, error });
   }
-};
+}; 
 
 
 
-const categoryController = {getCategoryById , createCategory, updateCategory, getAllCategory, deleteCategory};
+const categoryController = { getCategoryById, createCategory, updateCategory, deleteCategory ,getAllCategory};
 
 export default categoryController;
