dc236dc22249fb348f4786559ca59e8aa1fa71cf
Yasmine Hannouf <yasmina.hannouf.2018@gmail.com>
2023-04-13 11:28:24 +0300

    fixing getall function

--- Backend/routes/categoryRoutes.js
+++ Backend/routes/categoryRoutes.js
@@ -4,7 +4,7 @@ import Controller from "../controlles/categoryContoller.js";
 const router = express.Router();
 
 router.post("/", Controller.createCategory);
-router.get("/", Controller.getAllCategory);
+router.get("/all", Controller.getAllCategory);
 router.get("/:id", Controller.getCategoryById);
 router.put("/:id", Controller.updateCategory);
 router.delete("/:id", Controller.deleteCategory);
